5a2a7809b16266a684f3288e014daad1bf74342c
unknown <geet7896@gmail.com>
2018-08-22 16:17:33 +0530

    FirstChanges

---
+++ src/main/java/Test5/Test5/App.java
@@ -0,0 +1,13 @@
+package Test5.Test5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
